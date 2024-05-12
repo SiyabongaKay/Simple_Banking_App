64c52622a6b19a567bc4f100ea496f0e70301306
skhebevu023a <skhebevu023a@student.wethinkcode.co.za>
2024-05-12 02:29:52 +0200

    Created User Class

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,44 @@
 package model;
 
+import java.util.UUID;
+
 public class User {
+    private UUID userID;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private Address address;
+
+    public User() {}
+    public User(String username, String password, String firstName, String lastName, String email, String phone) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.address = new Address();
+    }
+
+    public UUID getUserID() {return this.userID;}
+
+    public String getUsername() {return this.username;}
+    public String getFirstName() {return this.firstName;}
+    public String getLastName() {return this.lastName;}
+
+    public String getPassword() {return this.password;}
+    public void setPassword(String password) {this.password = password;}
+
+    public String getPhone() {return this.phone;}
+    public void setPhone(String phone) {this.phone = phone;}
+
+    public Address getAddress() {return this.address;}
+
+    public String getEmail() {return this.email;}
+    public void setEmail(String email) {this.email = email;}
+
+
 }
