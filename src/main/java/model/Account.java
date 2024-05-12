b628a1cc8cea13a89c21932419da1979be389d8a
skhebevu023a <skhebevu023a@student.wethinkcode.co.za>
2024-05-12 02:56:37 +0200

    Created Address,Account,Status Class

---
+++ src/main/java/model/Account.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.util.UUID;
+
+public class Account {
+    private UUID userID;
+    private String accountType;
+    private int accountNumber;
+    private double balance;
+    private Status status;
+
+    public Account(UUID userID, String accountType){
+        this.userID = userID;
+        this.accountType = accountType;
+        this.status = Status.ACTIVE;
+        this.accountNumber = generateAccountNumber();
+        this.balance = 0;
+    }
+
+    public Status getStatus() {return this.status;}
+    public String getAccountType() {return this.accountType;}
+    public int getAccountNumber() {return this.accountNumber;}
+    public UUID getUserID() {return this.userID;}
+
+    public double getAvailableBalance() {
+        return this.balance;
+    }
+
+    public int generateAccountNumber() {
+        //TODO
+        return 0;
+    }
+
+    public void deposit(double amount) {this.balance += amount;}
+    public void withdraw(double amount) {this.balance -= amount;}
+
+
+}
